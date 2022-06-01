0acfc7c0e2bdc326a7f333a8b1854831294fd3a7
mustafa <mustafaegebozkay555a@gmail.com>
2022-06-01 13:12:51 +0300

    PyQt-PySide-Custom-Widgets axample is added

---
+++ custom_widgets/custom_widgets.py
@@ -0,0 +1,6 @@
+# Run this from your terminal or create a python file, 
+# paste this code, then run
+# pip install QT-PyQt-PySide-Custom-Widgets
+
+from Custom_Widgets.ProgressIndicator import test
+test.main() 
